e3e9d2cdc988d65680b629acfbbcbe1e6a21b1ec
cheng-hsin <s950091qq@gmail.com>
2022-12-19 18:13:29 +0800

    update children

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,10 @@
-export default function List({ children }) {
+import React, { ReactNode } from "react";
+interface Props {
+  children?: ReactNode
+  // any props that come into the component
+}
+export default function List({ children }:Props) {
+  
     return (
       <ul className="divide-y divide-slate-100">
         {children}
